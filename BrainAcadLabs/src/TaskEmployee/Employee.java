6ab20c85828549611a3c1bd850353a63b1c502bf
a40in <a40in@AntonSorokin>
2016-04-17 20:08:37 +0300

    Task2 - complete

--- BrainAcadLabs/src/TaskEmployee/Employee.java
+++ BrainAcadLabs/src/TaskEmployee/Employee.java
@@ -1,5 +1,20 @@
 package TaskEmployee;
 
 public class Employee {
+	String name;
+	double[] salary;
 
+	public Employee(String name, double... salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+	
+
+	public void calcSalary(String name, double... salary) {
+		double totalSalary = 0.0;
+		for (double s : salary) {
+			totalSalary += s;
+		}
+		System.out.println("The total salary of " + name + " is " + totalSalary);
+	}
 }
